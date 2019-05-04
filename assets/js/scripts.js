c79e90b61aab963f2a2a1e1e5fa87060a9ce8116
jmatam5k <jmatam.go@gmail.com>
2019-05-04 03:06:35 -0400

    adding experience section, fixing styles

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,10 +2,17 @@ $(document).ready(function($) {
 	console.log("ready!!!");
 	$('#seeMore1').click(function(event) {
 		/* Act on the event */
-		$('.modal-body').html('Hola')
+		$('.modal-title').html('Titulo 1');
+		$('.modal-body').html('Contenido 1');
 	});
 	$('#seeMore2').click(function(event) {
 		/* Act on the event */
-		$('.modal-body').html('Hola 2')
+		$('.modal-title').html('Titulo 2');
+		$('.modal-body').html('Contenido 2');
+	});
+	$('#seeMore3').click(function(event) {
+		/* Act on the event */
+		$('.modal-title').html('Titulo 3');
+		$('.modal-body').html('Contenido 3');
 	});
 });
