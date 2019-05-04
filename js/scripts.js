df8b1eb588411b1b0bc37e9c1c53686d2824ba9f
Jennifer Matamala <jmatam.go@gmail.com>
2019-05-04 00:27:56 -0400

    fixing modal, and adding typeform

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,11 @@
 $(document).ready(function($) {
 	console.log("ready!!!");
-
+	$('#seeMore1').click(function(event) {
+		/* Act on the event */
+		$('.modal-body').html('Hola')
+	});
+	$('#seeMore2').click(function(event) {
+		/* Act on the event */
+		$('.modal-body').html('Hola 2')
+	});
 });
